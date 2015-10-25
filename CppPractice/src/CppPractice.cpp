3081f207abaa21d8cdee34356b6a168f63cb0d91
shanealv <shane.alv@gmail.com>
2015-10-25 13:17:08 -0600

    Mingw Commit

--- CppPractice/src/CppPractice.cpp
+++ CppPractice/src/CppPractice.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : CppPractice.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
